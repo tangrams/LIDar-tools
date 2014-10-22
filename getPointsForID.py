21f4adb1e761b68df1a5c872fda0b81279e9c0e1
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-10-22 13:38:12 -0400

    adding arguments

--- getPointsForID.py
+++ getPointsForID.py
@@ -4,21 +4,22 @@ import sys
 DATABASE = 'test2'
 BUFFER_POLY = 5 # 0 == disable BUFFER (extend the radius of the search)
 FILLING_DIST = 0 # 0 == disable filling 
-CENTERED = True
+CENTERED = False
+GROUND = 0
 
 def extractPolygon(osm_id,outfile):
 	connection = psycopg2.connect(database = DATABASE)
 	cursor = connection.cursor();
 
 	# Default variables
-	ground = 100000
 	centroid = (0,0)
-	ceiling = 0
+	height = 0
 
 	# Center the feature
 	if(CENTERED):
 		cursor.execute('SELECT ST_X(ST_Centroid(way)), ST_Y(ST_Centroid(way)) FROM planet_osm_polygon WHERE osm_id ='+osm_id)
 		centroid = cursor.fetchone()
+		print 'Points will be centered to ' + str(centroid[0]) +','+ str(centroid[1])
 
 	# Get the OSM Polygon
 	cursor.execute('SELECT ST_AsText( way ) FROM planet_osm_polygon WHERE osm_id ='+osm_id)
@@ -28,7 +29,7 @@ def extractPolygon(osm_id,outfile):
 		polygonPts.append([float(coor) for coor in point.split(" ")])
 
 	cursor.execute('SELECT height FROM planet_osm_polygon WHERE osm_id ='+osm_id)
-	ceiling = cursor.fetchone()[0]
+	height = cursor.fetchone()[0]
 
 	# Extend borders
 	if(BUFFER_POLY == 0):
@@ -42,29 +43,24 @@ def extractPolygon(osm_id,outfile):
 	# 	Write output file
 	outFile = open(outfile, 'w')
 	for row in cursor:
-		if (int(row[2]) < ground):
-			ground = int(row[2])
-		newLine = '%.2f %.2f %.2f' % (row[0]-centroid[0], row[1]-centroid[1], row[2])
-		outFile.write(newLine+'\n')
-
-	if (FILLING_DIST > 0):
-		for z in xrange(int(ground),int(ceiling)):
-			# print z
-			for p in polygonPts:
-				newLine = '%.2f %.2f %.2i' % (float(p[0])-float(centroid[0]), float(p[1])-float(centroid[1]), z+10.0)
-				# print newLine
-				outFile.write(newLine+'\n')
+		if (int(row[2]) >= GROUND):
+			newLine = '%.2f %.2f %.2f' % (row[0]-centroid[0], row[1]-centroid[1], row[2])
+			outFile.write(newLine+'\n')
 
 	outFile.close()
 
 if __name__ == '__main__':
 	total = len(sys.argv)
-	assert total >= 3, 'Usage: python getPointsForID.py osm_id_number outfile.xyz [--center]'
-
-	for arg in xrange(total):
-		if( sys.argv[arg]=='--center'):
-			CENTERED = True
+	assert total >= 3, 'Usage: '+ sys.argv[0] +' <osm_id_number> <outfile>.xyz [-c / --center] [-g / --ground <meters>]'
 
 	osm_id = sys.argv[1]
 	outfile = sys.argv[2]
-	extractPolygon(osm_id,outfile)
+	
+	for arg in xrange(total):
+		if( sys.argv[arg] in ('--center', '-c') ):
+			CENTERED = True 
+		elif (sys.argv[arg] in ('--ground', '-g')):
+			GROUND = int(sys.argv[arg+1])
+			print 'Ground set to ' + sys.argv[arg+1]
+
+	extractPolygon(osm_id,outfile)	
